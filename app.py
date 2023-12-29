c90f64d580749eba310e1c98756cd6de1699da91
Utkarsh Ranjan <utr5991@gmail.com>
2023-12-29 13:20:06 +0530

    Added sqlite3 binary

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import platform
+if platform.system() == 'Linux':
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import pandas as pd
 from streamlit_card import card
